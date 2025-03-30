0317bc22f897fd2ec37c238c95819c0b2ce72fc9
Vivisus <baharkalayci167@gmail.com>
2025-03-30 20:37:01 +0300

    Oyundaki zamanda sıçramayı yapacak olan kod
    Bu kodda oyundaki zamanda sıçrama kodlanmaktadır. Oyunda ana karakter saat nesnesini kullanarak geçmiş gelecek arasındaki geçişi yapacaktır. Is in past değişkeni ile sahne kontrolü yapılmaktadır. Geçmişte ise geçmişteki nesneler gelecekte ise gelecekteki nesneler gösterilecektir. Zamanda sıçra fonksiyonunda gwçmiş gelecek kontrolü yapılmaktadır.

---
+++ Saat.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class Saat : MonoBehaviour  // Bu kısımdaki saat objesi karakterin zamanda sıçrama yapacağı nesnedir
+{
+    public GameObject pastObjects;  
+    public GameObject futureObjects; 
+
+    private bool isInPast = false;  // Şu an geçmişte mi onu kontrol ediyoruz
+
+    private void OnMouseDown() 
+    {
+        ZamandaSicra();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0) 
+        {
+            Touch dokunma = Input.GetTouch(0);
+            Vector2 dokunmaKonumu = Camera.main.ScreenToWorldPoint(dokunma.position);
+            Collider2D tıklananObje = Physics2D.OverlapPoint(dokunmaKonumu);
+
+            if (tıklananObje != null && tıklananObje.gameObject == gameObject)
+            {
+                ZamandaSicra();
+            }
+        }
+    }
+
+    void ZamandaSicra()
+    {
+        isInPast = !isInPast;
+
+        foreach (GameObject obj in pastObjects)
+        {
+            obj.SetActive(isInPast);  
+        }
+
+        foreach (GameObject obj in futureObjects)
+        {
+            obj.SetActive(!isInPast); 
+        }
+
+        Debug.Log("Zaman değişti! Yeni zaman: " + (isInPast ? "Geçmiş" : "Gelecek"));
+    }
+}
