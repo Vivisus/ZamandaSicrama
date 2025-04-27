da3a198eed2cfc12d183e831890b690b691ba07a
Vivisus <baharkalayci167@gmail.com>
2025-04-27 20:57:48 +0300

    Zaman_sicrama kodundaki ufak hataların düzeltilmesi.cs
    Önceki haftalarda zamanda sıçrama saat objesine dokunulduğunda çalışacak şekilde düzenlemeler yapılmıştı. TimeTravel sınıfında toggle() fonksiyonunu silmeyi unuttum. Ayrıca saat.cs kodunda zamandasicra() fonksiyonunun çağırılabilmesi için fonksiyon public fonksiyon haline geldi.

--- Zaman_sicrama.cs
+++ Zaman_sicrama.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TimeTravel : MonoBehaviour
@@ -12,53 +12,26 @@ public class TimeTravel : MonoBehaviour
     public Light sceneLight; // Ortam ışığı
     public Color pastLightColor; // Geçmiş için ışık rengi (Örn: Sarımsı)
     public Color futureLightColor; // Gelecek için ışık rengi (Örn: Mavimsi)
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            Vector2 tiklanmaKonumu = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(tiklanmaKonumu, Vector2.zero);
 
-            if (hit.collider != null && hit.collider.gameObject == gameObject)
-            {
-                ToggleTimeTravel();
-            }
-        }
 
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
-
-                if (hit.collider != null && hit.collider.gameObject == gameObject)
-                {
-                    ToggleTimeTravel();
-                }
-            }
-        }
-    }
-
-   void ZamandaSicra()
+    public void ZamandaSicra()
     {
         isInPast = !isInPast;
-    
+
         foreach (GameObject obj in pastObjects)
         {
             obj.SetActive(isInPast);
         }
-    
+
         foreach (GameObject obj in futureObjects)
         {
             obj.SetActive(!isInPast);
         }
-    
+
         background.sprite = isInPast ? pastBackground : futureBackground;
         sceneLight.color = isInPast ? pastLightColor : futureLightColor;
-    
+
         Debug.Log("Zaman değişti! Şu an: " + (isInPast ? "Geçmiş" : "Gelecek"));
     }
 }
